05ede65b070fe7f81c6d4b13fb213a6d7e39e623
Andeson-jnr <andethaddeus1@gmail.com>
2023-10-06 19:58:47 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the lowercase alphabet, followed by a new line,
+ * using only the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char letter = 'a';
